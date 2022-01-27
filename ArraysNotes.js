8d36303520f280e6b52b3531561ca64bb835dd77
nic0mp <nico.mp215@gmail.com>
2022-01-27 13:47:43 -0600

    updates note

--- ArraysNotes.js
+++ ArraysNotes.js
@@ -4,3 +4,22 @@ const listItem = famousSayings[0]
 console.log(listItem);
 console.log(famousSayings[2]);
 console.log(famousSayings[3]);
+
+// UPDATE ELEMENTS
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados'
+console.log(groceryList);
+// OUTPUT [ 'bread', 'avocados', 'milk' ]
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+condiments[0] = 'Mayo'
+console.log(condiments);
+condiments = ['Mayo'];
+console.log(condiments);
+utensils[3] = 'Spoon';
+console.log(utensils);
+// OUTPUT: [ 'Mayo', 'Mustard', 'Soy Sauce', 'Sriracha' ]
+// [ 'Mayo' ]
+// [ 'Fork', 'Knife', 'Chopsticks', 'Spoon' ]
