df25ffbd7bd66e50a144c038a41e80f3d55a8f94
nic0mp <nico.mp215@gmail.com>
2022-01-27 23:18:11 -0600

    arrs and funcs

--- ArraysNotes.js
+++ ArraysNotes.js
@@ -54,3 +54,18 @@ console.log(groceryList.slice(1, 4));
 console.log(groceryList);
 const pastaIndex = groceryList.indexOf('pasta');
 console.log(pastaIndex);
+
+// Arrays and functions
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept);
+function removeElement(newArr) {
+  newArr.pop();
+};
+removeElement(concept);
+console.log(concept);
