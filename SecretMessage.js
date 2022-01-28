ab8914c4d7485ded225985c56353cb9bff0f330e
nic0mp <nico.mp215@gmail.com>
2022-01-28 16:10:35 -0600

    secret message

---
+++ SecretMessage.js
@@ -0,0 +1,6 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'right';
+// secretMessage[secretMessage.indexOf('easily')] = 'right';
+console.log(secretMessage.indexOf('first'))
